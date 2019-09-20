18945bc751c5ac7ee87ac3d00d4b52f7ede875cd
Phippsaurus <philipp.trucksaess@gmail.com>
2019-09-20 19:21:16 -0400

    Add '-' to calculator example

--- lr/examples/calculator.rs
+++ lr/examples/calculator.rs
@@ -8,15 +8,17 @@ grammar! {
     Id: f64;
 
     /r"[\n\s]+" => _;
-    /r"-?\d+(\.\d+)?" => Id |input: &str| input.parse::<f64>().unwrap();
+    /r"\d+(\.\d+)?" => Id |input: &str| input.parse::<f64>().unwrap();
 
     S ::= E '$' |e: f64| e;
     E ::= E '+' T |e: f64, t: f64| e + t;
+    E ::= E '-' T |e: f64, t: f64| e - t;
     E ::= T |t: f64| t;
     T ::= T '*' F |t: f64, f: f64| t * f;
     T ::= T '/' F |t: f64, f: f64| t / f;
     T ::= F |f: f64| f;
     F ::= Id |id: f64| id;
+    F ::= '-' Id |id: f64| -id;
     F ::= '(' E ')' |e: f64| e;
 }
 
